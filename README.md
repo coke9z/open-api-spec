41017bca4716fc8a7c9849c1fc1d43d75701d1a5
kosaka <kosaka@corinmail.com>
2023-01-17 19:02:28 +0900

    自動テストの追加(途中)

--- README.md
+++ README.md
@@ -1,25 +1,38 @@
 # toranomaki-sample
 
 ## OpenAPI
+
 - OpenAPI yamlファイル <br>
- `./toranomaki-sample-api/src/main/resources/api.yaml`
+  `./toranomaki-sample-api/src/main/resources/api.yaml`
 
-#### 動作確認 手順
+#### ■ インターフェースの自動生成～動作確認 手順
+
 1. コードの自動生成
-   - ```$ mvn claen install -f ./toranomaki-sample-api/pom.xml```
+    - ```$ mvn claen install -f ./toranomaki-sample-api/pom.xml```
 2. jarの生成
-   - ```$ mvn clean install -f ./toranomaki-sample-impl/pom.xml```
+    - ```$ mvn clean install -f ./toranomaki-sample-impl/pom.xml```
 3. Dockerの起動
-   - ```$ docker-compose up -d --build```
+    - ```$ docker-compose up -d --build```
 4. curlで叩いてみる(例)
-   - ```$ curl -s -X POST http://localhost:81/sample?sampleQuery=1230 -H "Content-Type: application/json" -H "SAMPLE-HEADER: hoge" -d '{"message":"Hello World!!"}'```
+    - ```$ curl -s -X POST http://localhost:81/sample?sampleQuery=1230 -H "Content-Type: application/json" -H "SAMPLE-HEADER: hoge" -d '{"message":"Hello World!!"}'```
 5. Responseの確認(例)
-   - ```{"message":"Hello World!!","header":"hoge","query":1230}```
+    - ```{"message":"Hello World!!","header":"hoge","query":1230}```
 
+#### ■ yamlファイルからRedoc(API仕様書)の生成
 
+- 前準備 プロジェクトルートで`redoc-cli`をインスコ
+  ```$ npm install```
+
+1. htmlファイル生成
+    - ```redoc-cli bundle ./toranomaki-sample-api/src/main/resources/api.yaml --output api_reference.html```
+2. ブラウザでhtmlファイルを開く
+
+#### ■ 自動テスト 手順
+
+1. TODO テスト用にrefがないAPIを登録取得削除分作る
 
 TODO
-- 自動テスト
+
 - MockServer
 - コードからOASの生成
 
---
+++ openapi-to-postmanv2/index.js
@@ -0,0 +1,103 @@
+const fs = require('fs');
+const Collection = require('postman-collection').Collection;
+
+// 必要に応じてPathは変更
+const yamlPath = '../toranomaki-sample-api/src/main/resources/api.yaml';
+const readFileData = fs.readFileSync(yamlPath, {encoding: 'UTF8'});
+
+const updateRequestQuery = (request, key, value) => {
+  const findQueryParam = request.url.query.find(
+      queryParam => queryParam.key === key);
+  findQueryParam.update({
+    key: key,
+    value: value
+  });
+}
+
+const updateRequestHeader = (request, key, value) => {
+  const findHeaderParam = request.headers.find(
+      headerParam => headerParam.key === key);
+  findHeaderParam.update({
+    key: key,
+    value: value
+  });
+}
+
+const updateRequestBody = (request, body) => {
+  request.body.update(body);
+}
+
+const updateRequest = (request) => {
+
+  // サンプルAPI
+  if (request.url.getPath().includes('sample')) {
+    updateRequestQuery(request, 'value', 'hogehoge');
+    updateRequestHeader(request, 'SAMPLE-HEADER', 'fugafuga');
+    updateRequestBody(request,
+        fs.readFileSync('./test_file/sample_api_request.json',
+            {encoding: 'UTF8'}));
+  }
+
+  return request;
+}
+
+const updateItem = (item) => {
+
+  if (item.items) {
+    item.items.all().forEach(item => updateItem(item));
+  } else {
+    // 追加
+    item.events.add({
+      listen: 'test',
+      script: {
+        exec: "pm.test('response 200 test', () => {\n"
+            + "    pm.response.to.have.status(200);\n"
+            + "});"
+      },
+      type: 'text/javascript'
+    });
+
+    item.request = updateRequest(item.request);
+  }
+
+  return item;
+}
+
+// yamlファイルを読み込んでPostman CollectionsのJSONファイルを生成する
+// const Index = require('openapi-to-postmanv2');
+const Index = require('openapi-to-postmanv2');
+const newman = require("newman");
+Index.convert({type: 'string', data: readFileData}, {},
+    (err, conversionResult) => {
+      if (!conversionResult.result) {
+        console.error('変換エラー', conversionResult.reason);
+        return;
+      }
+      console.log("1. yaml読み込み");
+      const convertData = conversionResult.output[0].data;
+      const collection = new Collection(convertData);
+
+      // Requestの書き換え
+      console.log("2. Request書き換え");
+      collection.items.all().forEach(item => updateItem(item))
+
+      // Postman CollectionsのJSONファイルを生成する
+      console.log("3. JSONファイル生成")
+      fs.writeFile('./output_json/postman-collections.json',
+          JSON.stringify(collection, null, 4), (err) => {
+            if (err) {
+              console.error(err)
+            }
+          });
+
+      // console.log("4. テスト");
+      // newman.run({
+      //   collection: collection,
+      //   reporters: 'cli',
+      //   environment: require('./test_file/local.postman_environment.json')
+      // }, (err) => {
+      //   if (err) console.error(err);
+      // });
+
+    }
+);
